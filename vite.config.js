86a6c17b1f8dbfb2730778eb109e3b4033c119ec
not gonna tell ya <eedduxx@gmail.com>
2024-09-05 01:54:25 -0500

    Name switch on vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/Proyecto_1_Sociotegnologia_juego/',
+  base:'/ST-GAME1/',
 })
