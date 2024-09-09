0c75a98dab5d913e1ca6eaeb27a11e11183e8122
Sebastian Romero <nakosume@gmail.com>
2024-09-08 20:31:37 -0500

    backgrounds

--- src/App.jsx
+++ src/App.jsx
@@ -142,7 +142,7 @@ function App() {
   const [title, setTitle] = useState()
   const [text, setText] = useState()
   const [backgroundColor, setBackgroundColor] = useState('');
-  const [background, setBackground] = useState('./imgs/inn.jpg');
+  const [background, setBackground] = useState('/imgs/inn.jpg');
   const [intro, setIntro] = useState(true)
   const [count1, setCount1] = useState(0);
   const [label1, setLabel1] = useState('Jugador 1')
@@ -164,7 +164,7 @@ function App() {
     setTitle(good[rand].titulo)
     setText(good[rand].descripcion)
     setBackgroundColor('green')
-    setBackground('./imgs/forest.jpg')
+    setBackground('/imgs/forest.jpg')
   };
 
   const handleBadButtonClick = () => {
@@ -172,7 +172,7 @@ function App() {
     setTitle(bad[rand].titulo)
     setText(bad[rand].descripcion)
     setBackgroundColor('red')
-    setBackground('./imgs/hell.jpg')
+    setBackground('/imgs/hell.jpg')
   };
 
   const handleChallengeButtonClick = () => {
@@ -180,7 +180,7 @@ function App() {
     setTitle(challenge[rand].titulo)
     setText(challenge[rand].descripcion)
     setBackgroundColor('purple')
-    setBackground('./imgs/nether.jpg')
+    setBackground('/imgs/nether.jpg')
   };
 
   const handleIntroDisplay = () => {
@@ -195,7 +195,7 @@ function App() {
         :<Toggle clickAction={handleIntroDisplay} textContent={'Volver a instrucciones'}></Toggle>
       }
       {intro
-        ? (<div className='game' style={{ backgroundColor, backgroundImage: 'url(./imgs/inn.jpg)', backgroundSize: 'cover' }}>
+        ? (<div className='game' style={{ backgroundColor, backgroundImage: 'url(/imgs/inn.jpg)', backgroundSize: 'cover' }}>
           <h1>Instrucciones</h1>
           <h3>Insert instructions here</h3>
         </div>)
